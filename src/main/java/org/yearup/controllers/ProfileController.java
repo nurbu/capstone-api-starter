ecac627e3bb9c7159d19a16fd1edd170e6e46c57
Nurbu <lamanurbu2@gmail.com>
2026-06-25 14:49:13 -0400

    Created the ProfileController with the class annotations.

---
+++ src/main/java/org/yearup/controllers/ProfileController.java
@@ -0,0 +1,13 @@
+package org.yearup.controllers;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("profile")
+@CrossOrigin
+@PreAuthorize("isAuthenticated()")
+public class ProfileController {
+}
