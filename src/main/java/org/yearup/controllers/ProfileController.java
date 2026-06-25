a6c0e9a2c267435582c12cfd925900c364562fbf
Nurbu <lamanurbu2@gmail.com>
2026-06-25 15:06:03 -0400

    Added the viewProfile method route and getByUserId to get the profile of the id.

--- src/main/java/org/yearup/controllers/ProfileController.java
+++ src/main/java/org/yearup/controllers/ProfileController.java
@@ -2,12 +2,35 @@ package org.yearup.controllers;
 
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.yearup.models.Profile;
+import org.yearup.models.User;
+import org.yearup.service.ProfileService;
+import org.yearup.service.UserService;
+
+import java.security.Principal;
 
 @RestController
 @RequestMapping("profile")
 @CrossOrigin
 @PreAuthorize("isAuthenticated()")
 public class ProfileController {
+    private ProfileService profileService;
+    private UserService userService;
+
+    public ProfileController(ProfileService profileService, UserService userService) {
+        this.profileService = profileService;
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public Profile viewProfile(Principal principal) {
+        String userName = principal.getName();
+        User user = userService.getByUserName(userName);
+        int userId = user.getId();
+
+        return profileService.getByUserId(userId);
+    }
 }
